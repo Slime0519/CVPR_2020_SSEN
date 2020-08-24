0d8c74da7fc95dd61107e2267bc59a8387322a5a
JunmyeongLee_GIST <slime0519@gist.ac.kr>
2020-08-24 16:00:00 +0900

    add conv routine

--- SSEN.py
+++ SSEN.py
@@ -29,15 +29,19 @@ class Deformable_Conv_Block(nn.Module):
     def __init__(self,input_channels):
         super(Deformable_Conv_Block, self).__init__()
         self.offset_estimator = Dynamic_offset_estimator(input_channelsize=input_channels*2)
+        self.offset_conv = nn.Conv2d(in_channels=input_channels*2, out_channels=input_channels*2*9)
         self.deformconv = DeformConv2d(in_channels=input_channels,out_channels=input_channels, kernel_size=3, padding=1, bias=False)
 
     def forward(self,lr_features, hr_features,showmode = False,num_block =None):
         input_offset = torch.cat((lr_features,hr_features),dim=1)
 
         estimated_offset = self.offset_estimator(input_offset)
+        estimated_offset = self.offset_conv(estimated_offset)
 
         if showmode:
             showpatch(estimated_offset,foldername="extracted_offset_deformconv{}".format(num_block))
+        print(estimated_offset.shape)
+        print(hr_features.shape)
         output = self.deformconv( x = hr_features, offset = estimated_offset)
 
         return output

7de4d99d112d1c062d355ab7b4390dfb2ec0cc1c
JunmyeongLee_GIST <slime0519@gist.ac.kr>
2020-08-27 15:12:56 +0900

    edit utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,9 @@ import PIL.Image as Image
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-import torch
+import cv2
+from Data_gen import Dataset_Test
+from torch.utils.data import DataLoader
 
 def showpatch(imagepatch, foldername=None, istensor = True):
     batchsize = imagepatch.shape[0]
@@ -27,27 +29,6 @@ def showpatch(imagepatch, foldername=None, istensor = True):
             plt.imshow(image, 'gray')
             plt.savefig(os.path.join(folderpath,"image{}.png".format(channel)))
 
-def saveoffset(offsetbatch, foldername = None):
-    offsetbatch = np.array(offsetbatch.detach())
-    offsetbatch = np.transpose(offsetbatch,(0,2,3,1))
-
-    sizetemp = offsetbatch.shape[:-1]
-    offset_coord = np.zeros((*sizetemp,int(offsetbatch.shape[-1]/2),2), dtype=np.float32)
-    """
-    for y in range(offset_coord.shape[1]):
-        for x in range(offset_coord.shape[2]):
-            for i in range(offset_coord.shape[3]):
-                coordtuple = offsetbatch[:,y,x,i*2:(i+1)*2]
-                offset_coord[:,y,x,i] = coordtuple
-    """
-    for i in range(offset_coord.shape[3]):
-        coordtuple = offsetbatch[:, :,:, i * 2:(i + 1) * 2]
-        offset_coord[:, :, :, i] = coordtuple
-
-    for i in range(offsetbatch.shape[0]):
-        np.save(os.path.join(foldername, "offset_{}.npy".format(i)),offsetbatch[i])
-   # print(offset_coord[:,0,0,:])
-
 def regularization_image(image):
     min = np.min(image)
     temp_image = image-min
@@ -59,29 +40,74 @@ def regularization_image(image):
 
 
 def getPSNR(image1, image2):
+    shape = image1.shape[0]
+
+#    MSE = (image1-image2)**2
+ #   MSE = np.sum(MSE)
+
+   # MSE = MSE/(shape**2)
     MSE = (np.square(image1-image2)).mean(axis = None)
+
     PSNR = 10*np.log10(255**2/MSE)
     return PSNR
+
+def pointdot(img, coordtuple = (10,10)):
+    dpi = 10
+
+    # Set red pixel value for RGB image
+    red = [1, 0, 0]
+    height, width, bands = img.shape
+
+    # Update figure size based on image size
+    figsize = width / float(dpi), height / float(dpi)
+
+    # Create a figure of the right size with one axes that takes up the full figure
+    figure = plt.figure(figsize=figsize)
+    axes = figure.add_axes([0, 0, 1, 1])
+
+    # Hide spines, ticks, etc.
+    axes.axis('off')
+
+    # Draw a red dot at pixel (62,62) to (66, 66)
+    #for i in range(62, 67):
+     #   for j in range(62, 67):
+      #    img[i][j] = red
+    img[coordtuple[0]:coordtuple[0]+5,coordtuple[1]:coordtuple[1]+5,:] = red
+    # Draw the image
+    axes.imshow(img, interpolation='nearest')
+    plt.show()
+    #figure.savefig("test.png", dpi=dpi, transparent=True)
+
+#def pointdeformoffset():
+
+
 
 if __name__ == "__main__":
     """
     array1 = np.random.rand(3,100,100)
     array1 = np.expand_dims(array1,axis=0)
-    
+
     array2 = np.random.rand(3, 100, 100)
     array2 = np.expand_dims(array2,axis=0)
     array3 = np.concatenate((array1,array2),axis=0)
     print(array3.shape)
     showpatch(array3,foldername="test",istensor=False)
+    
+    testset_dirpath = "CUFED_SRNTT/CUFED5"
+    Test_Dataset = Dataset_Test(dirpath=testset_dirpath, upscale_factor=4, mode="XH")
+    Test_Dataloader = DataLoader(dataset=Test_Dataset, shuffle=False, batch_size=1, num_workers=0)
+
+    for i,(input, target, ref) in enumerate(Test_Dataloader):
+        if i == 0:
+            continue
+        inputarray = np.array(ref.cpu().detach())
+        inputarray = np.transpose(np.squeeze(inputarray), (1, 2, 0))
+        print(inputarray.shape)
+        inputarray = regularization_image(inputarray)
+        pointdot(inputarray)
+
+        break
     """
-
-    array1 = torch.zeros([2,18,40,40],dtype = torch.float32)
-    for i in range(9):
-        array1[0,2*i:2*(i+1),:,:] = i
-    for i in range(9):
-        array1[1,2*i:2*(i+1),:,:] = 8-i
-   # print(array1[:,:,0,0])
-    saveoffset(array1)
-
-
-
+    offset1= np.load("Network_patches/offset/offset_deformconv1.npy")
+    offset1 =
+    print(offset1)

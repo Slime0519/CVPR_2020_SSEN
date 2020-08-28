b54ff191d4ba96c85f7b99b765263f4138657d4e
JunmyeongLee_GIST <slime0519@gist.ac.kr>
2020-08-28 18:14:07 +0900

    edit Test routine

--- Test.py
+++ Test.py
@@ -5,6 +5,7 @@ from Baseline_big import BigBaseline
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import matplotlib.image as pltimage
+import argparse
 
 from utils import regularization_image, getPSNR
 import numpy as np
@@ -19,15 +20,40 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
 
-
+parser = argparse.ArgumentParser(description="RefSR Network with SSEN Training module")
+parser.add_argument('--model_size', type = str, default="normal", help = "select model size")
 
 if __name__ == "__main__":
+
+    opt = parser.parse_args()
+
+    Modelsize = opt.model_size
+
+    if Modelsize == "normal":
+        prefix_resultname = "normalModel"
+    elif Modelsize == "big":
+        prefix_resultname = "bigModel"
+    else:
+        prefix_resultname = "smallModel"
+
     testset_dirpath = "CUFED_SRNTT/CUFED5"
 
-    model_dirpath = "Trained_model"
+    model_dirpath = os.path.join("Trained_model",prefix_resultname)
     model_epoch = 400
 
     image_savepath = "result_image"
+
+
+    if not os.path.isdir(os.path.join("Result_image",prefix_resultname)):
+        os.mkdir(os.path.join("Result_image",prefix_resultname))
+    if not os.path.isdir(os.path.join("Result_image",prefix_resultname,"input")):
+        os.mkdir(os.path.join("Result_image",prefix_resultname,"input"))
+    if not os.path.isdir(os.path.join("Result_image",prefix_resultname,"output")):
+        os.mkdir(os.path.join("Result_image",prefix_resultname,"output"))
+    if not os.path.isdir(os.path.join("Result_image",prefix_resultname,"reference")):
+        os.mkdir(os.path.join("Result_image",prefix_resultname,"reference"))
+    if not os.path.isdir(os.path.join("Result_image",prefix_resultname,"target")):
+        os.mkdir(os.path.join("Result_image",prefix_resultname,"target"))
 
     testmodel = Baseline().to(device)
 #    testmodel = BigBaseline().to(device)
@@ -35,7 +61,7 @@ if __name__ == "__main__":
     Test_Dataloader = DataLoader(dataset=Test_Dataset, shuffle=False, batch_size=1, num_workers=0)
 
     # original saved file with DataParallel
-    state_dict = torch.load(os.path.join(model_dirpath,"Larger_Model_epoch{}.pth".format(model_epoch)))
+    state_dict = torch.load(os.path.join(model_dirpath,prefix_resultname+"_epoch{}.pth".format(model_epoch)))
     # create new OrderedDict that does not contain `module.`
     from collections import OrderedDict
 
@@ -51,6 +77,7 @@ if __name__ == "__main__":
   #      torch.load(os.path.join(model_dirpath,"larger_Model_epoch{}.pth".format(model_epoch))))
     #testmodel = testmodel.to(device)
     testmodel.eval()
+    PSNRarr = np.zeros(len(Test_Dataloader))
 
     for i, (input, target, refimage) in enumerate(Test_Dataloader):
         # if not i == 33:
@@ -78,6 +105,7 @@ if __name__ == "__main__":
         PSNR = getPSNR(regularized_output_image, target_image)
 
         print("PSNR : {}".format(PSNR))
+        PSNRarr[i] = PSNR
         input_temp = np.array(input.cpu().detach())
         input_bicubic = cv2.resize(np.transpose(np.squeeze(input_temp), (1, 2, 0)), dsize=(0, 0), fx=4, fy=4,
                                    interpolation=cv2.INTER_CUBIC)
@@ -89,21 +117,21 @@ if __name__ == "__main__":
 
         # PNG Image 저장
         PIL_Input_Image = Image.fromarray(regularized_input_image)
-        PIL_Input_Image.save("Result_image/Larger_model1/input/image{}.png".format(i))  # save large size image
+        PIL_Input_Image.save(os.path.join("Result_image",prefix_resultname,"input/image{}.png".format(i)))  # save large size image
         #PIL_Input_Image.save("Result_image/input/image{}.png".format(i))
         
         PIL_output_Image = Image.fromarray(regularized_output_image)
-        PIL_output_Image.save("Result_image/Larger_model1/output/image{}.png".format(i))
+        PIL_output_Image.save(os.path.join("Result_image",prefix_resultname,"output/image{}.png".format(i)))
         #PIL_Input_Image.save("Result_image/output/image{}.png".format(i))
 
         PIL_ref_Image = Image.fromarray(regularized_ref_image)
         #PIL_Input_Image.save("Result_image/reference/image{}.png".format(i))
-        PIL_ref_Image.save("Result_image/Larger_model1/reference/image{}.png".format(i))
+        PIL_ref_Image.save(os.path.join("Result_image",prefix_resultname,"reference/image{}.png".format(i)))
 
         PIL_target_Image = Image.fromarray(target_image)
         #PIL_Input_Image.save("Result_image/target/image{}.png".format(i))
-        PIL_target_Image.save("Result_image/Larger_model1/target/image{}.png".format(i))
+        PIL_target_Image.save(os.path.join("Result_image",prefix_resultname,"target/image{}.png".format(i)))
 
-
+        np.save(os.path.join("Result_image",prefix_resultname,"{}_PSNRlist.npy".format(prefix_resultname)),PSNRarr)
 
 

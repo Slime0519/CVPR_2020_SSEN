18553f6d308638bb8d51ecc22d23e189c67c2cc1
JunmyeongLee_GIST <slime0519@gist.ac.kr>
2020-09-22 17:34:50 +0900

    add EDSR baseline

---
+++ Models/Train/EDSR_baseline.py
@@ -0,0 +1,90 @@
+import torch.nn as nn
+import torch
+from Models.Train_utils import make_residual_block
+
+import torch.nn as nn
+from torchsummary import summary
+
+def make_model(args, parent=False):
+    return EDSR(args)
+
+class EDSR(nn.Module):
+    def __init__(self, ):
+        super(EDSR, self).__init__()
+
+
+        act = nn.ReLU(True)
+
+       # self.sub_mean = common.MeanShift(args.rgb_range)
+     #   self.add_mean = common.MeanShift(args.rgb_range, sign=1)
+
+        # define head module
+        m_head = [nn.Conv2d(in_channels=3, out_channels=64,kernel_size=3,padding=1, bias=True)]
+      #  m_head = nn.Conv2d(in_channels=128, out_channels=64, kernel_size = 3, padding=1)
+        # define body module
+        """
+        m_body = [
+            common.ResBlock(
+                conv, n_feats, kernel_size, act=act, res_scale=args.res_scale
+            ) for _ in range(n_resblocks)
+        ]
+        
+        m_body.append(conv(n_feats, n_feats, kernel_size))
+        """
+        self.body = make_residual_block(blocknum=16,input_channel=64,output_channel=64)
+        # define tail module
+        """
+        m_tail = [
+            common.Upsampler(conv, scale, n_feats, act=False),
+            conv(n_feats, args.n_colors, kernel_size)
+        ]
+        """
+        m_tail = [
+            nn.Conv2d(in_channels=64, out_channels=4 * 64, kernel_size=3, padding=1, bias=True),
+            nn.PixelShuffle(2),
+            nn.Conv2d(in_channels=64, out_channels=3, kernel_size=3, padding=1, bias=True)
+        ]
+
+
+
+        self.head = nn.Sequential(*m_head)
+        self.tail = nn.Sequential(*m_tail)
+
+    def forward(self, x):
+      #  x = self.sub_mean(x)
+        x = self.head(x)
+
+        res = self.body(x)
+        res += x
+
+        x = self.tail(res)
+        #x = self.add_mean(x)
+
+        return x
+
+    def load_state_dict(self, state_dict, strict=True):
+        own_state = self.state_dict()
+        for name, param in state_dict.items():
+            if name in own_state:
+                if isinstance(param, nn.Parameter):
+                    param = param.data
+                try:
+                    own_state[name].copy_(param)
+                except Exception:
+                    if name.find('tail') == -1:
+                        raise RuntimeError('While copying the parameter named {}, '
+                                           'whose dimensions in the model are {} and '
+                                           'whose dimensions in the checkpoint are {}.'
+                                           .format(name, own_state[name].size(), param.size()))
+            elif strict:
+                if name.find('tail') == -1:
+                    raise KeyError('unexpected key "{}" in state_dict'
+                                   .format(name))
+
+
+if __name__ == "__main__":
+    model = EDSR()
+    dict= torch.load("edsr_baseline_x2-1bc95232.pt")
+    model.load_state_dict(dict,strict=False)
+    summary(model, (3,160,160),device="cpu")
+
